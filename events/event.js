823a67dc21333d789f17fa603a0ac24e59f8533d
eshaalal <eshalal888@gmail.com>
2024-01-30 19:33:46 +0530

    changes

---
+++ events/event.js
@@ -0,0 +1,23 @@
+// var eve = require('events');
+// var obj=new eve.EventEmitter;
+// obj.addListener("dancing",function(msg)
+// {
+//     console.log(msg);
+// })
+// obj.emit("dancing","lets dance");
+// obj.emit("dancing","lets dance");
+// obj.emit("dancing","lets dance");
+//addlistener will call every call
+//once will call once 
+//on will call every time 
+
+//create a event named play, register callback function, run the event.
+// var play = require('events');
+// var obj=new play.EventEmitter;
+// obj.addListener("playing",function(msg)
+// {
+//     console.log(msg);
+// })
+// obj.emit("playing","lets play");
+// obj.emit("playing","lets play");
+//we can create this using function also
